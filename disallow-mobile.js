8e4c71c61d0d43eea0380a4c55b2505fd7cf3bd2
Suraj Sharma <suraj@evenzero.in>
2025-10-14 12:40:28 +0530

    fix: disallow-mobile

---
+++ disallow-mobile.js
@@ -0,0 +1,18 @@
+(function() {
+    const isMobile = {
+        Android: () => navigator.userAgent.match(/Android/i),
+        BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
+        iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
+        Opera: () => navigator.userAgent.match(/Opera Mini/i),
+        Windows: () => navigator.userAgent.match(/IEMobile/i),
+        any: function() {
+            return (this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows());
+        }
+    };
+
+    if (isMobile.any() || window.innerWidth <= 768) {
+        document.body.style.display = 'none';
+        alert('This website is not accessible from mobile devices. Please use a desktop computer.');
+        window.location.href = 'https://evenzero.in';
+    }
+})();
