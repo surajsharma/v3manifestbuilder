d9af1300a2e0a7c57513ea299b4ab67ac49b5cee
Suraj Sharma <suraj@evenzero.in>
2025-10-14 16:19:27 +0530

    fix: dark mode

--- disallow-mobile.js
+++ disallow-mobile.js
@@ -1,20 +1,25 @@
-(function() {
-    console.log("is mobile?")
-    const isMobile = {
-        Android: () => navigator.userAgent.match(/Android/i),
-        BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
-        iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
-        Opera: () => navigator.userAgent.match(/Opera Mini/i),
-        Windows: () => navigator.userAgent.match(/IEMobile/i),
-        any: function() {
-            return (this.Android() || this.BlackBerry() || this.iOS() || this.Opera() || this.Windows());
-        }
-    };
+(function () {
+  const isMobile = {
+    Android: () => navigator.userAgent.match(/Android/i),
+    BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
+    iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
+    Opera: () => navigator.userAgent.match(/Opera Mini/i),
+    Windows: () => navigator.userAgent.match(/IEMobile/i),
+    any: function () {
+      return (
+        this.Android() ||
+        this.BlackBerry() ||
+        this.iOS() ||
+        this.Opera() ||
+        this.Windows()
+      );
+    },
+  };
 
-    if (isMobile.any() || window.innerWidth <= 768) {
-        alert('This website is not accessible from mobile devices. Please use a desktop computer.');
-        window.location.href = 'https://evenzero.in';
-    } else {
-        console.log('not mobile');
-    }
+  if (isMobile.any() || window.innerWidth <= 768) {
+    alert(
+      "This website is not accessible from mobile devices. Please use a desktop computer."
+    );
+    window.location.href = "https://evenzero.in";
+  }
 })();
